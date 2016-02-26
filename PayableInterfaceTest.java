4f30117a03bfc929eaad387837ae8ce5f361ee42
ramandip <ca.ramandip@gmail.com>
2016-02-25 19:29:35 -0500

    Uploaded
    Example of interface& inheritance

---
+++ PayableInterfaceTest.java
@@ -0,0 +1,19 @@
+// Fig. 10.15: PayableInterfaceTest.java
+// Payable interface test program processing Invoices and
+// Employees polymorphically.
+public class PayableInterfaceTest
+{
+	public static void main(String[] args)
+	{
+		// create four-element Payable array
+
+
+		// populate array with objects that implement Payable
+
+		
+		System.out.println(
+				"Invoices and Employees processed polymorphically:");
+		// generically process each element in array payableObjects
+
+	} // end main
+} // end class PayableInterfaceTest
